c3e765b71fdca45bc2da53c17ba90deb2a4cec2f
michael.ambrus <michael.ambrus@sigmaconnectivity.se>
2016-03-04 16:16:54 +0100

    Buspirate: much improved stability
    State-changes & synchronisation.

--- src/devices/buspirate.c
+++ src/devices/buspirate.c
@@ -43,7 +43,8 @@ static regex_t preg;            /* Compiled regular expression for full
 ")"
 
 /* Commands in binary mode. Commands prefixed "ENTER" continues to further
- * submode. Not all commands are defines. Full definition here:
+ * submode. Note: Not all commands are defined here, only mode-changers (so
+ * far). Full definition here:
  * http://dangerousprototypes.com/docs/Bitbang#Commands
  */
 typedef enum {
@@ -64,7 +65,7 @@ struct cmdrply_s {
 };
 
 struct cmdrply_s cmdrply[] = {
-    {ENTER_RESET, "BBIO0"},
+    {ENTER_RESET, "BBIO1"},     /*<- Note: protocol-version specific */
     {ENTER_SPI, "SPI1"},
     {ENTER_I2C, "I2C1"},
     {ENTER_UART, "ART1"},
@@ -75,6 +76,11 @@ struct cmdrply_s cmdrply[] = {
 };
 
 #define TBL_LEN (sizeof(cmdrply) / sizeof(struct cmdrply_s))
+#define BUF_SZ 100
+#define STATE_RETRIES 10
+#define US_DELAY_RETRY 10000    /* Must be long enough to allow any ongoing
+                                   replies to reach UART registers. I.e.
+                                   Baud-rate dependent. */
 
 /* Driver companion - NOTE: unique for each driver. Must NOT be public */
 struct ddata {
@@ -93,6 +99,8 @@ static int rawMode_enter(struct device *);
 static int rawMode_toMode(struct device *, bpcmd_t bpcmd);
 static void empty_inbuff(int fd);
 static int read_2err(int fd, char *rbuff, int len);
+static char *expected_rply(int cmd);
+static unsigned int lookup_cmd(char *rply);
 
 /* Convenience variable: Bus-pirate provides one API. Variants communicated
  * through ddata */
@@ -244,7 +252,10 @@ int buspirate_deinit_device(struct device *device)
     LOGI("BP: Destroying device ID [%d]\n", device->devid);
     empty_inbuff(ddata->fd);
     ASSURE(rawMode_toMode(device, ENTER_RESET) == 0);
+    usleep(10000);
+    empty_inbuff(ddata->fd);
     ASSURE(rawMode_toMode(device, RESET_BUSPIRATE) == 0);
+    usleep(100000);
 
     close(ddata->fd);
     free(ddata);
@@ -291,16 +302,38 @@ static void empty_inbuff(int fd)
     int ret, tries = 0;
     char tmp[3];
     do {
-        ASSURE_E((ret = read_2err(fd, tmp, 1)) != -1, log_ioerror(errno));
+        ret = read_2err(fd, tmp, 1);
+        if ((ret == -1) && (errno != EAGAIN))
+            log_ioerror(errno);
         tries++;
     } while (ret > 0);
+}
+
+static char *expected_rply(int cmd)
+{
+    int i;
+    for (i = 0; i < TBL_LEN; i++) {
+        if (cmdrply[i].command == cmd)
+            return (char *)cmdrply[i].rply;
+    }
+    return NULL;
+}
+
+static unsigned int lookup_cmd(char *rply)
+{
+    int i;
+    for (i = 0; i < TBL_LEN; i++) {
+        if (strcmp(cmdrply[i].rply, rply) == 0)
+            return cmdrply[i].command;
+    }
+    return 0xDEAD;
 }
 
 /* Enter binary mode from normal console-mode */
 static int rawMode_enter(struct device *device)
 {
-    int ret, i;
-    char tmp[100] = { '\0' };
+    int ret, i, corr_cmd;
+    char tmp[BUF_SZ] = { '\0' };
     int done = 0;
     int tries = 0;
     struct ddata *ddata = device->driver->ddata;
@@ -313,9 +346,13 @@ static int rawMode_enter(struct device *device)
         return -1;
     }
 
-    tmp[0] = 0x00;
+    /* Do the first 20 writes without listening for any reply. This is
+     * according to BusPirate protocol spec V1.
+     */
     for (i = 0; i < 20; i++) {
+        tmp[0] = 0x00;
         LOGD("Sending 0x%02X to port\n", tmp[0]);
+        usleep(1000);
         ASSURE_E((ret = write(*fd, tmp, 1)) != -1, log_ioerror(errno));
     }
 
@@ -326,13 +363,16 @@ static int rawMode_enter(struct device *device)
         ASSURE_E((ret = write(*fd, tmp, 1)) != -1, log_ioerror(errno));
         tries++;
         LOGD("tries: %i Ret %i\n", tries, ret);
-        usleep(1);
+        usleep(1000);
         ASSURE_E((ret = read_2err(*fd, tmp, 5)) != -1, log_ioerror(errno));
         if (ret != 5 && tries > 22) {
-            LOGE("Buspirate did not respond correctly (%i,%i) \n", ret, tries);
+            corr_cmd = lookup_cmd(tmp);
+            LOGE("Buspirate did not respond correctly in function [%s] (%i,%i)\n", __func__, ret, tries);
+            LOGE("  Command requested: 0x%02X\n", 0x00);
+            LOGE("  Reply matches:     0x%02X\n", corr_cmd);
             return -1;
         } else if (strncmp(tmp, "BBIO1", 5) == 0) {
-            /*Empty any remains in buffer */
+            /* Empty any remains in buffer */
             empty_inbuff(*fd);
             done = 1;
         }
@@ -341,6 +381,8 @@ static int rawMode_enter(struct device *device)
             LOGE("Buspirate: - chip not detected, or not readable/writable\n");
             return -1;
         }
+        usleep(US_DELAY_RETRY);
+        empty_inbuff(*fd);
     }
 
     ddata->state = ENTER_RESET;
@@ -349,35 +391,51 @@ static int rawMode_enter(struct device *device)
 
 static int rawMode_toMode(struct device *device, bpcmd_t bpcmd)
 {
-    int ret, i, slen;
-    char tmp[100] = { '\0' };
+    int ret, i, slen, corr_cmd, tries = 0;
+    char tmp[BUF_SZ] = { '\0' };
     char *expRply = NULL;
     struct ddata *ddata = device->driver->ddata;
     int *fd = &ddata->fd;
 
-    tmp[0] = bpcmd;
+    LOGI("BusPirate entering mode %d\n", bpcmd);
+
     for (i = 0; i < TBL_LEN; i++) {
         if (cmdrply[i].command == bpcmd)
             expRply = (char *)cmdrply[i].rply;
     }
+    expRply = expected_rply(bpcmd);
     if (expRply == NULL) {
         LOGE("BusPirate drive doesn't know what response to expect for cmd %d",
              bpcmd);
         return -1;
     }
 
-    LOGD("Sending 0x%02X to port. Expecting response %s\n", tmp[0], expRply);
-    write(*fd, tmp, 1);
-    usleep(1);
-    slen = strlen(expRply);
-    ret = read(*fd, tmp, slen);
+    for (tries = 0; tries < STATE_RETRIES; tries++) {
+        memset(tmp, 0, BUF_SZ);
+        tmp[0] = bpcmd;
+        LOGD("Sending 0x%02X to port. Expecting response %s\n", tmp[0],
+             expRply);
+        ASSURE_E((ret = write(*fd, tmp, 1)) != -1, log_ioerror(errno));
+        usleep(1000);
+        slen = strlen(expRply);
+        ASSURE_E((ret = read_2err(*fd, tmp, slen)) != -1, log_ioerror(errno));
 
-    if ((ret == strlen(expRply)) && (strncmp(tmp, expRply, slen) == 0)) {
-        ddata->state = bpcmd;
-        return 0;
+        if ((ret == strlen(expRply)) && (strncmp(tmp, expRply, slen) == 0)) {
+            ddata->state = bpcmd;
+            empty_inbuff(*fd);
+            return 0;
+        }
+        usleep(US_DELAY_RETRY);
+        empty_inbuff(*fd);
+        LOGD("Retry (%d) due to Buspirate unexpected response to "
+             "mode-change: (%i,%s) \n", tries + 1, ret, tmp);
     }
 
-    LOGE("Buspirate did not respond correctly: (%i,%s) \n", ret, tmp);
+    corr_cmd = lookup_cmd(tmp);
+    LOGE("Buspirate did not respond correctly in function [%s] (%i,%i)\n",
+         __func__, ret, tries);
+    LOGE("  Command requested: 0x%02X\n", bpcmd);
+    LOGE("  Reply matches:     0x%02X\n", corr_cmd);
     return -1;
 }
 
