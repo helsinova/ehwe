7a0065e27c4ea7fcd8777dd498288aba890c2fc7
Michael Ambrus <michael.ambrus@gmail.com>
2017-11-28 12:05:08 +0100

    Wiki Githubbyfied
    Change-Id: I31bbbf7afc1514c91e5db8397599292c021222e3

--- wiki/start_wiki.sh
+++ wiki/start_wiki.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+THIS_WIKI_DIR=$(dirname $(readlink -f $0))
+
 if [ -f ~/.cabal/bin/gitit ]; then
 	echo "Will start with local-build gitit binary... (preferred)"
 	GITIT_BIN=~/.cabal/bin/gitit
@@ -14,15 +16,26 @@ if [ "X${GITIT_BIN}" == "X" ]; then
 	exit 1
 fi
 
-#get config port:
-CPORT=$(grep -e'^port:[[:space:]]' wiki_config.txt | cut -f2 -d" ")
-
 if [ "X$(which screen 2>/dev/null)" == "X" ]; then
 	echo "Error: screen needed to run service. Please install..."
 	exit 1
 fi
 
-echo "Gitit starting local webserver at http://127.0.0.1:${CPORT} in screen"
-screen -dmS "wiki-$(basename $(pwd))" ${GITIT_BIN} -f wiki_config.txt -l 127.0.0.1
-echo "To enter local screen:"
-echo "  screen -rd \"wiki-$(basename $(pwd))\""
+pushd ${THIS_WIKI_DIR} >/dev/null
+	#get config port:
+	CPORT=$(grep -e'^port:[[:space:]]' wiki_config.txt | cut -f2 -d" ")
+	SESSION_NAME="wiki-$(basename $(cd ../; pwd))"
+	echo
+	echo "Gitit starting local webserver at http://127.0.0.1:${CPORT} in screen"
+	screen -dmS "${SESSION_NAME}" ${GITIT_BIN} -f wiki_config.txt -l 127.0.0.1
+	echo "To enter local screen (debugging):"
+	echo "  screen -rd \"${SESSION_NAME}\""
+	echo
+	if	[ "X$(uname -a | grep -i CYGWIN)" != "X" ]; then
+		cygstart http://localhost:${CPORT}
+	else
+		xdg-open http://localhost:${CPORT}
+	fi
+popd >/dev/null
+
+echo "Opened start-page for you in local browser..."
