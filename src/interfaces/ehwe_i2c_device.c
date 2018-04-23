da52f2098b5254b3ddc66cb35d47b5966229420a
Michael Ambrus <michael.ambrus@terranet.se>
2018-04-23 09:07:31 +0200

    i2c-device abstraction added
    Convenience functions to work with i2c-devices on a device level
    instead of having to use both bus and address and manage
    start/stop conditions.
    
    This originates from the BP_i2c_pca9685_pwm branch i.e.
    ehwe_workbench.git (i.e. the embedded/ directory from public
    sub-module).

---
+++ src/interfaces/ehwe_i2c_device.c
@@ -0,0 +1,162 @@
+/***************************************************************************
+ *   Copyright (C) 2018 by Michael Ambrus                                  *
+ *   michael@helsinova.se                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/*
+ * Device level i2c, i.e. chip level, abstraction
+ *
+ * This is a high abstraction level which builds on Nordics i2c API
+ *
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <ehwe.h>
+#include <ehwe_i2c_device.h>
+
+/* Forward declaration of layout for device registers.
+	 Layout differs from IC to IC and is thus unknown to this
+	 abstraction-level
+ */
+struct devregs;
+
+struct i2c_device_struct {
+    /* HW-bus: I2C0-I2Cn */
+    I2C_TypeDef *bus;
+
+    /* 7-bit write address - read-address implicit */
+    uint8_t addr;
+
+    /* this-pointer */
+    struct i2c_device_struct *self;
+
+    /* Copy of IC:s all registers or NULL if not in sync. Handling of this
+     * member is up to the device-driver and can be optional. */
+    struct i2c_device_devregs *reg;
+};
+
+/* Creates a i2c-device instance, and returns handle to it. */
+i2c_device_hndl i2c_device_open(I2C_TypeDef * bus, uint8_t addr)
+{
+    struct i2c_device_struct *i2c_device;
+
+    i2c_device = malloc(sizeof(struct i2c_device_struct));
+
+    /* Set this-pointer */
+    i2c_device->self = i2c_device;
+
+    /* bus-id for this instance. */
+    i2c_device->bus = bus;
+
+    /* Chip adress (7-bit form) */
+    i2c_device->addr = addr;
+
+    /* Register map, uninitialized */
+    i2c_device->reg = NULL;
+
+    return i2c_device;
+}
+
+/* Destroy i2c-device instance */
+void i2c_device_close(i2c_device_hndl i2c_device)
+{
+    if (i2c_device->reg != NULL)
+        free(i2c_device->reg);
+
+    i2c_device->self = NULL;
+    i2c_device->addr = 0xF7;    /* 8-bit magic number fo debugging memory leaks */
+    i2c_device->reg = NULL;
+
+    free(i2c_device);
+}
+
+uint8_t i2c_device_read_uint8(i2c_device_hndl i2c_device, uint8_t reg)
+{
+    uint8_t val = 0;
+
+    /* Send which register to access, omit STOP */
+    i2c_write(i2c_device->bus, i2c_device->addr, (uint8_t[]) {
+              reg}, 1, 0);
+
+    i2c_read(i2c_device->bus, i2c_device->addr, &val, sizeof(val));
+
+    return val;
+}
+
+uint16_t i2c_device_read_uint16(i2c_device_hndl i2c_device, uint8_t reg)
+{
+    uint16_t val = 0;
+    uint8_t buf[2];
+
+    /* Send which register to access, omit STOP */
+    i2c_write(i2c_device->bus, i2c_device->addr, (uint8_t[]) {
+              reg}, 1, 0);
+
+    i2c_read(i2c_device->bus, i2c_device->addr, buf, sizeof(val));
+    val = *(uint16_t *)buf;
+
+    return val;
+}
+
+uint32_t i2c_device_read_uint32(i2c_device_hndl i2c_device, uint8_t reg)
+{
+    uint32_t val = 0;
+    uint8_t buf[4];
+
+    /* Send which register to access, omit STOP */
+    i2c_write(i2c_device->bus, i2c_device->addr, (uint8_t[]) {
+              reg}, 1, 0);
+
+    i2c_read(i2c_device->bus, i2c_device->addr, buf, sizeof(val));
+    val = *(uint32_t *)buf;
+
+    return val;
+}
+
+void i2c_device_write_uint8(i2c_device_hndl i2c_device, uint8_t reg,
+                            uint8_t val)
+{
+    /* Send which register to access directly followed by value */
+    i2c_write(i2c_device->bus, i2c_device->addr, (uint8_t[]) {
+              reg, val}, sizeof(val) + 1, 1);
+}
+
+void i2c_device_write_uint16(i2c_device_hndl i2c_device, uint8_t reg,
+                             uint16_t val)
+{
+    uint8_t buf[sizeof(val) + 1];
+    buf[0] = reg;
+    *(uint16_t *)(&buf[1]) = val;
+
+    assert(sizeof(buf) == 3);
+
+    i2c_write(i2c_device->bus, i2c_device->addr, buf, sizeof(val) + 1, 1);
+}
+
+void i2c_device_write_uint32(i2c_device_hndl i2c_device, uint8_t reg,
+                             uint32_t val)
+{
+    uint8_t buf[sizeof(val) + 1];
+    buf[0] = reg;
+    *(uint32_t *)(&buf[1]) = val;
+
+    assert(sizeof(buf) == 5);
+
+    i2c_write(i2c_device->bus, i2c_device->addr, buf, sizeof(val) + 1, 1);
+}
---
+++ src/interfaces/include/ehwe_i2c_device.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2018 by Michael Ambrus                                  *
+ *   michael@helsinova.se                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef ehwe_i2c_device_h
+#define ehwe_i2c_device_h
+
+#include <stdint.h>
+
+/* Not strictly true as we will use Nordics higher-level API.
+ * Include is a work-around for EHWE not separating them well enough. This
+ * will change and might require a change of the include (TBD) */
+#include <stm32f10x.h>
+
+/* Forward declaration - hide unneeded details*/
+struct i2c_device_struct;
+typedef struct i2c_device_struct *i2c_device_hndl;
+
+/* Open/close - Creates/destroys a i2c-device instance */
+i2c_device_hndl i2c_device_open(I2C_TypeDef * bus, uint8_t addr);
+void i2c_device_close(i2c_device_hndl i2c_device);
+
+/* Register-level access functions for 8,16 & 32-bit access */
+uint8_t i2c_device_read_uint8(i2c_device_hndl, uint8_t reg);
+uint16_t i2c_device_read_uint16(i2c_device_hndl, uint8_t reg);
+uint32_t i2c_device_read_uint32(i2c_device_hndl, uint8_t reg);
+void i2c_device_write_uint8(i2c_device_hndl, uint8_t reg, uint8_t val);
+void i2c_device_write_uint16(i2c_device_hndl, uint8_t reg, uint16_t val);
+void i2c_device_write_uint32(i2c_device_hndl, uint8_t reg, uint32_t val);
+
+#endif                          //ehwe_i2c_device_h
